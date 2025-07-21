4a88a352be06f6e96a1f5aa335f462216f5980a9
Sumit <sumit500241@gmail.com>
2025-07-21 11:28:21 +0530

    File upload, delete and editor component

---
+++ components/editor.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { BlockNoteEditor, PartialBlock } from "@blocknote/core";
+import { useCreateBlockNote } from "@blocknote/react";
+import { BlockNoteView } from "@blocknote/mantine";
+import "@blocknote/core/style.css";
+import "@blocknote/mantine/style.css";
+import { useTheme } from "next-themes";
+import { useEffect } from "react";
+import { useEdgeStore } from "@/lib/edgestore";
+
+interface EditorProps {
+  onChange: (value: string) => void;
+  initialContent?: string;
+  editable?: boolean;
+}
+
+const Editor = ({ onChange, initialContent, editable }: EditorProps) => {
+  const { resolvedTheme } = useTheme();
+  const { edgestore } = useEdgeStore();
+
+  const handleUpload = async (file: File) => {
+    const response = await edgestore.publicFiles.upload({ file });
+    return response.url;
+  };
+
+  const editor: BlockNoteEditor = useCreateBlockNote({
+    initialContent: initialContent
+      ? (JSON.parse(initialContent) as PartialBlock[])
+      : undefined,
+    uploadFile: handleUpload,
+  });
+
+  useEffect(() => {
+    const handleChange = () => {
+      onChange(JSON.stringify(editor.document, null, 2));
+    };
+
+    const unsubscribe = editor.onChange(handleChange);
+
+    return () => {
+      if (unsubscribe) {
+        unsubscribe();
+      }
+    };
+  }, [editor, onChange]);
+
+  return (
+    <div>
+      <BlockNoteView
+        editor={editor}
+        editable={editable}
+        theme={resolvedTheme === "dark" ? "dark" : "light"}
+      />
+    </div>
+  );
+};
+
+export default Editor;
